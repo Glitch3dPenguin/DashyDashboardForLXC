8c92534c70ca0c1d14e925318a5446c4d470ddfc
Max Kulik <pearelectronics@live.com>
2023-05-26 10:49:42 -0500

    Update install-dashy-alpine3.16.sh
    Added line to create user "dashy" 
    Added line to create user group "dashy"
    Added args to boot script to assign it to user "dashy"
    
    This is an attempt to fix a problem where yarn is not "auto building" changes made to the config on the fly. It seems this may be an issue when running Yarn as root or a root user.

--- Alpine Scripts/install-dashy-alpine3.16.sh
+++ Alpine Scripts/install-dashy-alpine3.16.sh
@@ -3,50 +3,47 @@
 #Stop the script if there is an error.
 set -o errexit
 
+#Setting up some fun colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 #Initial system update
 apk update && apk upgrade
-echo System Updated
-sleep 2
-echo Installing Required Files Now
+echo -e "${RED}System Updated...${NC}"
+echo -e "${RED}Installing Required Files Now...${NC}"
 apk add git net-tools curl wget
-echo Required tools installed!
-sleep 2
-echo Running system update!
-sleep 2
+echo -e "${RED}Required tools installed...${NC}"
+echo -e "${RED}Running system update...${NC}"
 apk update && apk upgrade
-echo System update complete!
-sleep 2
-echo Installing NodeJS
-sleep 2 
+echo -e "${RED}System update complete...${NC}"
+echo -e "${RED}Installing NodeJS...${NC}"
 apk add --update nodejs=16.20.0-r0
-echo NodeJS Installed!
-sleep 2
-echo Installing Yarn
-sleep 2
+echo -e "${RED}NodeJS Installed...${NC}"
+echo -e "${RED}Installing Yarn...${NC}"
 apk add yarn
-echo Yarn installed!
-sleep 2 
-echo checking required versions
-echo Yarn Version:
+echo -e "${RED}Yarn installed...${NC}"
+echo -e "${RED}Checking required versions...${NC}"
+echo -e "Yarn Version:"
 yarn --version 
-echo Required: 1.22.19
-echo NodeJS Version:
+echo -e "Required: 1.22.19"
+echo -e "NodeJS Version:"
 node -v
-echo Required: 16.20.0
-echo If all the versions match then Dashy is ready to be installed!
+echo -e "Required: 16.20.0"
+echo -e "${RED}If all the versions match then Dashy is ready to be installed...${NC}"
 sleep 4
-echo Cloneing Dashy:
+echo -e "${RED}Cloneing Dashy...${NC}"
 git clone https://github.com/Lissy93/dashy.git
 cd /root/dashy/
-echo Dashy Cloned!
-sleep 2
+echo -e "${RED}Dashy Cloned...${NC}"
+echo -e "${RED}Creating Dashy User and User Group...${NC}"
+addgroup -S dashy && adduser -S dashy -G dashy
+echo -e "${RED}Dashy user has been created...${NC}"
 yarn # Install dependencies
 #Yarn will run out of memory when trying to build. This next command will allow 
 #NodeJS to build with a max size of 1000mb of memory. It will fail without it. 
 export NODE_OPTIONS=--max-old-space-size=1000
 #build Dashy 
 yarn build
-
 #Let's make Dashy run on system startup
 #Create simple launch script for Dashy
 cat <<EOF > /root/start.sh
@@ -54,7 +51,7 @@ cat <<EOF > /root/start.sh
 cd /root/dashy/
 yarn start #start the app
 EOF
-#Allow the to run
+#Set up Dashy to run at system boot
 chmod +x /root/start.sh
 
 #Create the service script for boot
@@ -62,10 +59,13 @@ cat <<EOF > /etc/init.d/dashy
 #!/sbin/openrc-run
 
 description="Running Dashy on system start"
+supervisor=supervise-daemon
+USER=dashy
 
 name="Dashy"
 command="/root/start.sh"
 command_background="yes"
+command_user="${USER}"
 pidfile="/var/run/dashy.pid"
 
 depend() {
@@ -79,9 +79,9 @@ chmod +x /etc/init.d/dashy
 rc-update add dashy
 rc-update add dashy boot
 
-echo If you see this message everything worked!
-echo Reboot container and launch
+echo -e "${RED}If you see this message everything worked!${NC}"
+echo -e "${RED}Reboot container and launch${NC}"
 ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
-echo on port 4000
+echo -e "on port 4000"
 rc-service dashy start
 exit 1
